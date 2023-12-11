9eef3af32255c9ccda7dee15cc9989584766270a
gabrielsousaf <gs294860@gmail.com>
2023-12-11 11:57:04 -0300

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import { useState } from 'react'
-
-import Header from './components/Header/Header'
-import Main from './components/Main/Main'
-import Footer from './components/Footer/Footer'
-import Modal from './components/Modal/Modal'
-import TaskForm from './components/TaskForm/TaskForm'
-
-import { ITask } from './interfaces/Task'
-
-// import './App.css'
+import DisplayWeather from './components/DisplayWeather';
 
 function App() {
-  
-  const [taskList, setTaskList] = useState<ITask[]>([]);
-
   return (
-    <div>
-      <Modal children={<TaskForm btnText='Editar Tarefa' taskList={taskList}  setTaskList={setTaskList}/>}/>
-      <Header/>
-      <Main/>
-      <Footer/>
+    <div className="App">
+      <DisplayWeather />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
